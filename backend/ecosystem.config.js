bc64f5d0427ed26cfce161565d289ce3beae5f39
Vsevolod Demidov <demidov.vsevolod@uep24.ru>
2025-05-26 16:59:57 +0300

    first commit

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -27,7 +27,7 @@ module.exports = {
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'ls && cd backend && ls && cd source && npm i && pm2 start ecosystem.config.js --env production && pm2 save',
+      'post-deploy': 'cd backend && npm i && pm2 start ecosystem.config.js --env production && pm2 save',
     },
   },
 };
