04e995bee06e996166d6e2de1ce30d2611fc34b3
Vsevolod Demidov <demidov.vsevolod@uep24.ru>
2025-05-26 16:34:49 +0300

    first commit

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -19,7 +19,7 @@ module.exports = {
             ref: DEPLOY_REF,
             repo: DEPLOY_REPO,
             path: DEPLOY_PATH,
-            'post-deploy': 'cd ../source/frontend && npm i && npm run build',
+            'post-deploy': 'npm i && npm run build',
         },
     },
 };
